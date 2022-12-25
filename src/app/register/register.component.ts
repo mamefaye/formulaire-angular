ece99c21ad077ec5c51f008c4d55986858a2f53e
mamefaye <mamefaye2017gueye@gmail.com>
2022-12-25 18:03:29 +0000

    formbuilder

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { User } from './user';
 
 @Component({
@@ -9,11 +9,36 @@ import { User } from './user';
 })
 export class RegisterComponent {
 
+  // public input = 'mmm';
+
   public user: User = new User();
 
-  public saveData(registerForm: NgForm) {
-    console.log(registerForm.form);
-    console.log('valeur', JSON.stringify(registerForm.value));
+  public registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: false
+    });
+  }
+
+  public saveData() {
+    console.log(this.registerForm);
+    console.log('valeur', JSON.stringify(this.registerForm.value));
     console.log('Hello');
+  }
+
+  public fillFormData() {
+    this.registerForm.patchValue({
+      firstName: 'Mame',
+      lastName: 'Faye',
+      sendCatalog: true
+    });
   }
 }
